5b4ad16d58da1004c4a202e0dd398210572e3b7c
Dimitrios Amaxilatis <d.amaxilatis@gmail.com>
2017-03-16 16:09:42 +0200

    add constants

---
+++ client/src/test/java/eu/organicity/client/test/Constants.java
@@ -0,0 +1,13 @@
+package eu.organicity.client.test;
+
+public class Constants {
+    static final String EXPERIMENTER_ID = "";
+    static final String EXPERIMENT_ID = "";
+    static final String EXPERIMENT_SCOPE = "private";
+    static final String APPLICATION_ID = "";
+
+    static String CLIENT_ID = "";
+    static String CLIENT_SECRET = "";
+    static String USERNAME = "";
+    static String PASSWORD = "";
+}
